6ac6bd50b9ea3cb625d461f1a54e49d2e8c60ce2
mynsun <alstjs3022@naver.com>
2025-06-02 05:22:09 +0000

    up

---
+++ sidebar/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from PIL import Image
+
+# -------------------------- Side Bar --------------------------
+st.sidebar.title("Sidebar")
+st.sidebar.header("텍스트 입력 사용 예")
+user_id = st.sidebar.text_input("아이디(ID) 입력", value = 'root', max_chars = 15)
+user_password = st.sidebar.text_input("패스워드(Passowrd)", value = '1234', type = 'password')
+
+st.sidebar.header("셀렉트 박스 사용 예")
+selectbox_option = ['진주 귀걸이를 한 소녀', '별이 빛나는 밤', '절규', '월화정인']
+your_option = st.sidebar.selectbox("좋아하는 작품은?", selectbox_option, index = 3)
+st.sidebar.write(" **당신의 선택** : ", your_option)
+
+# -------------------------- Side Bar --------------------------
+folder = './data/'
+
+image_files = [
+    "https://github.com/mynsun/streamlit/blob/main/sidebar/data/Vermeer.png?raw=true",
+    "https://github.com/mynsun/streamlit/blob/main/sidebar/data/Gogh.png?raw=true",
+    "https://github.com/mynsun/streamlit/blob/main/sidebar/data/Munch.png?raw=true",
+    "https://github.com/mynsun/streamlit/blob/main/sidebar/data/ShinYoonbok.png?raw=true",
+
+]
+
+selectbox_options_index = selectbox_option.index(your_option)
+img_file = image_files[selectbox_options_index]
+
+st.image(img_file, width = 400, caption = your_option)
